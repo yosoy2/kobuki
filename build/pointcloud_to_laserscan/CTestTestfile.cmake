8e075c98075f33ecdf731677504c47fb8e3510ec
edu <edotercero66.el@gmail.com>
2019-06-18 15:50:08 -0400

    se agrega repo pointcloud to laserscan pero aun no se consiguen puntos

---
+++ build/pointcloud_to_laserscan/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eduardo/ws/kobuki/src/pointcloud_to_laserscan
+# Build directory: /home/eduardo/ws/kobuki/build/pointcloud_to_laserscan
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_pointcloud_to_laserscan_roslint_package "/home/eduardo/ws/kobuki/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/eduardo/ws/kobuki/build/test_results/pointcloud_to_laserscan/roslint-pointcloud_to_laserscan.xml" "--working-dir" "/home/eduardo/ws/kobuki/build/pointcloud_to_laserscan" "--return-code" "/opt/ros/kinetic/share/roslint/cmake/../../../lib/roslint/test_wrapper /home/eduardo/ws/kobuki/build/test_results/pointcloud_to_laserscan/roslint-pointcloud_to_laserscan.xml make roslint_pointcloud_to_laserscan")
