405ea0f9f882417aedc449dc9912daab1efbf193
edu <edotercero66.el@gmail.com>
2019-08-23 17:57:52 -0400

    se cargan los mapas mapeados en rviz, pero aun no se prueban con koboki

---
+++ build/provide_map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eduardo/ws/kobuki/src/provide_map
+# Build directory: /home/eduardo/ws/kobuki/build/provide_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
